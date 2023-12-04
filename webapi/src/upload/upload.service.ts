56d1b039cdce527ffcb66eb5b0fff52c5968f7d9
Puneet Gupta <vishal.sharma@globalmediainsight.in>
2023-12-04 18:21:26 +0530

    1

--- webapi/src/upload/upload.service.ts
+++ webapi/src/upload/upload.service.ts
@@ -1,22 +1,25 @@
-import { S3Client ,PutObjectCommand} from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config/dist';
-
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class UploadService {    
-    private readonly    s3clint = new S3Client({
-        region:this.configService.getOrThrow('AWS_S3_REGION'),
-    }); 
-    constructor(private readonly configService:ConfigService){}
-    async upload(fileName: string, file: Buffer ,){
-        const response = await this.s3clint.send(
-            new PutObjectCommand({
-                Bucket:'nestimg',
-                Key: fileName,
-                Body: file,
-            }),
-        );
-        console.log('response' + response)
-    }
+export class UploadService {
+  private readonly s3client: S3Client;
+
+  constructor(private readonly configService: ConfigService) {
+    this.s3client = new S3Client({
+      region: this.configService.getOrThrow('AWS_S3_REGION'),
+    });
+  }
+
+  async upload(fileName: string, file: Buffer) {
+    const response = await this.s3client.send(
+      new PutObjectCommand({
+        Bucket: 'nestimg',
+        Key: fileName,
+        Body: file,
+      }),
+    );
+    console.log('response', response);
+  }
 }
