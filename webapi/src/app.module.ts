6c56f4b35e39f53473163e372cec2c8835893d08
Puneet Gupta <vishal.sharma@globalmediainsight.in>
2023-12-06 01:42:01 +0530

    1

--- webapi/src/app.module.ts
+++ webapi/src/app.module.ts
@@ -9,20 +9,24 @@ import { Employe } from './employe/employeentity/employe.entity';
 import { UploadModule } from './upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
 
-
-
-
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type:'mysql',
-    host:'localhost'||"127.0.0.1" ,
-    port:3306,
-    username:'root',
-    password:'sourav123',
-    database:'usersignup',
-    entities:[Users,Employe],
-    synchronize:true  
-  }), UserModule,EmployeModule, UploadModule,ConfigModule.forRoot({isGlobal:true})],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'nest-rest-api-database.co7kilirejkj.ap-south-1.rds.amazonaws.com',
+      port: 3306,
+      username: '`nestjsadmin`',
+      password: `sunshine1`, // Removed backticks around the password
+      database: 'usersignup',
+      entities: [Users, Employe],
+      synchronize: true,
+      logging: true, // Add this line for detailed logging
+    }),
+    UserModule,
+    EmployeModule,
+    UploadModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
